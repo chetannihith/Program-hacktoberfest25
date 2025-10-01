c78b321a877c56c35ce92cfb20f10b5f946e7b89
adityamep <akanonymous07@gmail.com>
2025-10-01 07:30:46 +0530

    Add Bubble Sort program in C++ with comments

---
+++ Cpp/bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// This program sorts an array using the Bubble Sort algorithm
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // Bubble Sort logic
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap elements if they are in wrong order
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
